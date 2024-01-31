7edbc7c40154164824900ed8c181fd4be66f152b
PawanAK <pawanajjark@gmail.com>
2024-01-31 18:29:07 +0530

    Add dotenv configuration and connect to MongoDB

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000; // Use the port from environment variable or default to 3000
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-const PORT = 3000;
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+io.on('connection', (socket) => {
+    console.log('A new client is connected');
 });
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to the database');
+        // Listen to port
+        http.listen(PORT, () => {
+            console.log('Listening for requests on port', PORT);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err);
+    });
